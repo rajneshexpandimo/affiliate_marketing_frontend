5433e446565c843876cb435673c5406fad3c86bd
Rajnesh Saini <rajnesh@team.expandimo.com>
2023-08-08 11:12:43 +0530

    Animation with responsive

--- components/home/cookiesless_future/index.jsx
+++ components/home/cookiesless_future/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import CountUp from "@/components/common/CountUp";
+import { motion } from "framer-motion";
 
 const CookieLessFuture = () => {
   const cardData = [
@@ -20,22 +21,50 @@ const CookieLessFuture = () => {
     },
   ];
 
+  const cardVariantsdown = {
+    offscreen: {
+      opacity: 0,
+      y: 50,
+    },
+    onscreen: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        duration: 0.4,
+      },
+    },
+  };
+
   return (
     <div className='bg-[url("/images/cookieless_banner.png")] bg-no-repeat bg-cover bg-top sm:py-[180px] py-[100px]'>
-      <div className="md:max-w-[1300px] w-full mx-auto  xl:px-0 px-5">
-        <h1 className="text-white font-bold sm:text-[50px] text-3xl sm:pb-5 pb-0 sm:leading-[66px] leading-normal text-center">
-          Show the cookieless future who&apos;s boss.
-        </h1>
-        <p className="text-center text-white sm:text-2xl text-[18px] font-medium">
-          Access the Sovrn Data Collective
-        </p>
+      <motion.div
+        className="card-container"
+        initial="offscreen"
+        whileInView="onscreen"
+        viewport={{ once: true, amount: 0.8 }}
+      >
+        <motion.div
+          variants={cardVariantsdown}
+          className="md:max-w-[1300px] w-full mx-auto  xl:px-0 px-5"
+        >
+          <h1 className="text-white font-bold sm:text-[50px] text-3xl sm:pb-5 pb-0 sm:leading-[66px] leading-normal text-center">
+            Show the cookieless future who&apos;s boss.
+          </h1>
+          <p className="text-center text-white sm:text-2xl text-[18px] font-medium">
+            Access the Sovrn Data Collective
+          </p>
           <div className="flex sm:flex-row flex-col justify-around sm:py-[100px] py-[30px] lg:max-w-[1030px] w-full mx-auto sm:gap-0 gap-10">
             {cardData.map((data, key) => (
               <CardComp key={key} {...data} index={key} />
             ))}
           </div>
-       <p className="text-white sm:text-2xl text-[18px] font-medium text-center lg:max-w-[1030px] w-full mx-auto">Tap the world&apos;s largest publisher collective for deep consumer insights and enriched audience data. Then get out there and rule the roost.</p>
-      </div>
+          <p className="text-white sm:text-2xl text-[18px] font-medium text-center lg:max-w-[1030px] w-full mx-auto">
+            Tap the world&apos;s largest publisher collective for deep consumer
+            insights and enriched audience data. Then get out there and rule the
+            roost.
+          </p>
+        </motion.div>
+      </motion.div>
     </div>
   );
 };
@@ -43,16 +72,18 @@ const CookieLessFuture = () => {
 const CardComp = (props) => {
   const { count, desc, suffix, index } = props;
   return (
-      <div className="inline-block">
-        <div className="border-2 border-dashed border-white p-2 rounded-full mx-auto min-w-[160px] min-h-[160px] max-w-[160px] max-h-[160px] flex justify-center items-center">
+    <div className="inline-block">
+      <div className="border-2 border-dashed border-white p-2 rounded-full mx-auto min-w-[160px] min-h-[160px] max-w-[160px] max-h-[160px] flex justify-center items-center">
         <p className="text-white sm:text-3xl text-lg font-medium bg-[#1F57A1] rounded-full  min-w-[134px] min-h-[134px] max-w-[134px] max-h-[134px] flex items-center justify-center">
           <CountUp end={count} />
           {suffix}
         </p>
-        </div>
-        <p className="text-white text-2xl text-center pt-2 font-medium"> {desc}</p>
       </div>
-    
+      <p className="text-white text-2xl text-center pt-2 font-medium">
+        {" "}
+        {desc}
+      </p>
+    </div>
   );
 };
 
--- components/home/earn_confidence/index.jsx
+++ components/home/earn_confidence/index.jsx
@@ -1,67 +1,86 @@
 import Image from "next/image";
 import React from "react";
-import { motion } from "framer-motion";
+import { motion, useScroll, Variants } from "framer-motion";
 
-const visible = { opacity: 1, x: 0, transition: { duration: 0.9 } };
-
-const itemVariants = {
-  hidden: { opacity: 0, y: 10 },
-  visible,
+const cardVariantsLeft = {
+  offscreen: {
+    opacity: 0,
+    x: -50,
+  },
+  onscreen: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      duration: 0.8,
+    },
+  },
+};
+const cardVariantsRight = {
+  offscreen: {
+    opacity: 0,
+    x: 50,
+  },
+  onscreen: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      duration: 0.8,
+    },
+  },
 };
 
 const EarnConfidence = () => {
+  const { scrollYProgress } = useScroll();
   return (
     <div className="earn_confidence lg:my-2 my-10">
-      <motion.article
-        initial="hidden"
-        animate="visible"
-        exit={{ opacity: 0, transition: { duration: 1 } }}
-        variants={{ visible: { transition: { staggerChildren: 0.3 } } }}
-      >
         <div className="md:max-w-[1300px] w-full mx-auto flex lg:flex-row flex-col xl:px-0 px-5 items-center">
           <div className="lg:w-1/2 w-full">
-            <motion.img
-              variants={{
-                hidden: { opacity: 0, x: -50 },
-                visible,
-              }}
-              src="/images/confidence.png"
-              alt="Earn with confidence"
-              width={627}
-              height={550}
-            />
+            <motion.div
+              className="card-container"
+              initial="offscreen"
+              whileInView="onscreen"
+              viewport={{ once: true, amount: 0.8 }}
+            >
+              <motion.img
+                variants={cardVariantsLeft}
+                src="/images/confidence.png"
+                alt="Earn with confidence"
+                width={627}
+                height={550}
+              />
+            </motion.div>
           </div>
           <div className="lg:w-1/2 w-full">
-            <motion.h1
-              variants={{
-                hidden: { opacity: 0, x: 50 },
-                visible,
-              }}
-              className="font-bold sm:text-[50px] text-3xl sm:pb-5 pb-0 sm:leading-[66px] leading-normal"
+            <motion.div
+              className="card-container"
+              initial="offscreen"
+              whileInView="onscreen"
+              viewport={{ once: true, amount: 0.8 }}
             >
-              <span className="text-[#295DA7]">Earn</span> with confidence
-            </motion.h1>
-            <motion.p
-              variants={{
-                hidden: { opacity: 0, x: 50 },
-                visible,
-              }}
-              className="text-base font-light leading-[21px]"
-            >
-              Here at SP, we are dedicated to providing unparalleled support to
-              our affiliates, ensuring their success and growth. Our tailored
-              approach begins by understanding the unique needs and goals of
-              each affiliate, allowing us to offer personalized guidance and
-              resources. We believe in fostering strong relationships with our
-              affiliates, providing continuous training and education on the
-              latest marketing strategies and industry trends. By equipping them
-              with cutting-edge tools and a user-friendly platform, we empower
-              our affiliates to optimize their performance and maximize their
-              earnings.
-            </motion.p>
+              <motion.h1
+                // variants={cardVariantsRight}
+                className="font-bold sm:text-[50px] text-3xl sm:pb-5 pb-0 sm:leading-[66px] leading-normal"
+              >
+                <span className="text-[#295DA7]">Earn</span> with confidence
+              </motion.h1>
+              <motion.p
+                // variants={cardVariantsRight}
+                className="text-base font-light leading-[21px]"
+              >
+                Here at SP, we are dedicated to providing unparalleled support
+                to our affiliates, ensuring their success and growth. Our
+                tailored approach begins by understanding the unique needs and
+                goals of each affiliate, allowing us to offer personalized
+                guidance and resources. We believe in fostering strong
+                relationships with our affiliates, providing continuous training
+                and education on the latest marketing strategies and industry
+                trends. By equipping them with cutting-edge tools and a
+                user-friendly platform, we empower our affiliates to optimize
+                their performance and maximize their earnings.
+              </motion.p>
+            </motion.div>
           </div>
         </div>
-      </motion.article>
     </div>
   );
 };

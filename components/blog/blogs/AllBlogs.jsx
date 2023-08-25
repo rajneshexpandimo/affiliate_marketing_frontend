f4804660b1f4c7c6346383d5e866141143ea9220
Rajnesh Saini <rajnesh@team.expandimo.com>
2023-08-25 18:55:09 +0530

    Blog Page done

---
+++ components/blog/blogs/AllBlogs.jsx
@@ -0,0 +1,90 @@
+import Image from "next/image";
+import React from "react";
+
+const cardData = [
+  {
+    image: "/images/blog1.jpg",
+    date: "February1,2023",
+    title: "The Future of Affiliate Marketing",
+    desc: "As a digital marketer, you’ve probably heard about broker affiliate programs.",
+    link: "https://www.google.com/maps/place/100+High+St,+Boston,+MA+02110,+USA/@42.3544742,-71.0578804,17z/data=!3m1!4b1!4m6!3m5!1s0x89e3708178ea0e83:0x50c89534c1717af0!8m2!3d42.3544742!4d-71.0553055!16s%2Fg%2F11cnddnv0y?entry=ttu",
+  },
+  {
+    image: "/images/blog2.jpg",
+    date: "February1,2023",
+    title: "The Future of Affiliate Marketing",
+    desc: "100 High StreetBoston, MA 02110",
+    link: "https://www.google.com/maps/place/100+High+St,+Boston,+MA+02110,+USA/@42.3544742,-71.0578804,17z/data=!3m1!4b1!4m6!3m5!1s0x89e3708178ea0e83:0x50c89534c1717af0!8m2!3d42.3544742!4d-71.0553055!16s%2Fg%2F11cnddnv0y?entry=ttu",
+  },
+  {
+    image: "/images/blog3.jpg",
+    date: "February1,2023",
+    title: "The Future of Affiliate Marketing",
+    desc: "100 High StreetBoston, MA 02110",
+    link: "https://www.google.com/maps/place/100+High+St,+Boston,+MA+02110,+USA/@42.3544742,-71.0578804,17z/data=!3m1!4b1!4m6!3m5!1s0x89e3708178ea0e83:0x50c89534c1717af0!8m2!3d42.3544742!4d-71.0553055!16s%2Fg%2F11cnddnv0y?entry=ttu",
+  },
+];
+
+const AllBlogs = () => {
+  return (
+    <div className="text-center">
+      <div className="text-left">
+        {" "}
+        {cardData.map((data, key) => (
+          <CardComp key={key} {...data} index={key} />
+        ))}
+      </div>
+      <button className="border-2 border-s-[21px] px-9 py-4 border-[#1F57A1] bg-[#E7E7E7] rounded-[5px] font-semibold uppercase mx-auto">
+        More details
+      </button>
+    </div>
+  );
+};
+
+const CardComp = (props) => {
+  const { image, title, date, desc, link, index } = props;
+  return (
+    <div className="w-full flex md:flex-row flex-col even:bg-white odd:bg-[#D3D3D3] even:border-b-2 border-[#33333333] even:border-0 mb-11">
+      <div className="relative ">
+        <Image
+          src={image}
+          width={353}
+          height={346}
+          alt={image}
+          className="min-w-[353px] min-h-[346px] object-cover"
+        />
+        <div className="flex absolute bottom-0 justify-between w-full bg-[#33333380] py-2 px-3 text-[#fff]">
+          <p className="w-1/2 flex items-center text-base gap-1">
+            <Image
+              src={"/icons/calendar.svg"}
+              width={20}
+              height={20}
+              alt="comments"
+            />{" "}
+            {date}
+          </p>
+          <p className="w-1/2 flex items-center text-base gap-1">
+            <Image
+              src={"/icons/comments.svg"}
+              width={20}
+              height={20}
+              alt="comments"
+            />{" "}
+            Comments
+          </p>
+        </div>
+      </div>
+      <div className="flex flex-col justify-center py:[69px] px-[44px]">
+        <h3 className="sm:text-3xl text-xl sm:leading-9 leading-none font-semibold text-[#333]">
+          {title}
+        </h3>
+        <p className="text-lg py-[22px]">{desc}</p>
+        <button className="bg-[#1F57A1] rounded-[7px] text-white text-lg max-w-[122px] h-[39px]">
+          Read More
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default AllBlogs;
---
+++ components/blog/blogs/BlogSidebar.jsx
@@ -0,0 +1,117 @@
+import React from "react";
+import { Input, Space, Dropdown, Typography } from "antd";
+import Image from "next/image";
+const { Search } = Input;
+
+const BlogSidebar = () => {
+  const categories = [
+    {
+      key: "1",
+      label: "Item 1",
+    },
+    {
+      key: "2",
+      label: "Item 2",
+    },
+    {
+      key: "3",
+      label: "Item 3",
+    },
+  ];
+  const tags = [
+    {
+      key: "1",
+      label: "Item 1",
+    },
+    {
+      key: "2",
+      label: "Item 2",
+    },
+    {
+      key: "3",
+      label: "Item 3",
+    },
+  ];
+
+
+  const onSearch = (value) => console.log(value);
+
+  return (
+    <div>
+      <div className="bg-[#d3d3d3] rounded-2xl py-[26px] px-[45px] mb-[48px]">
+        <h4 className="text-[#1F57A1] uppercase text-lg font-bold border-b-[1px] border-[#1F57A1] inline-block w-auto mb-5">
+          search by post
+        </h4>
+        <Search
+          placeholder="search..."
+          allowClear
+          enterButton="Search"
+          size="large"
+          onSearch={onSearch}
+          className="blog_search"
+        />
+      </div>
+      <div className="bg-[#d3d3d3] rounded-2xl py-[26px] px-[45px] mb-[48px]">
+        <h4 className="text-[#1F57A1] uppercase text-lg font-bold border-b-[1px] border-[#1F57A1] inline-block w-auto mb-5">
+          Categories
+        </h4>
+        <div className="w-full">
+          <Dropdown
+            menu={{
+                categories,
+              selectable: true,
+              defaultSelectedKeys: ["3"],
+            }}
+            trigger={["click"]}
+          >
+            
+            <Typography.Link className="bg-white w-full block rounded-[20px] p-[10px] ">
+              <Space className="w-full justify-between text-[#33333380]">
+                Select Category
+                <Image
+                  src={"/icons/blog_downArrow.svg"}
+                  width={17}
+                  height={17}
+                  alt="down_arrow"
+                />
+              </Space>
+            </Typography.Link>
+          </Dropdown>
+        </div>
+      </div>
+      <div className="bg-[#1F57A1] rounded-2xl py-[26px] px-[45px] mb-[36px]">
+      <h4 className="text-white uppercase text-lg font-bold border-b-[1px] border-[#fff] inline-block w-auto mb-5">
+          Recent Posts
+        </h4>
+       <p className="text-sm text-white font-bold my-3 capitalize flex items-start gap-2"><Image src={'/icons/RightButton.svg' } width={24} height={24} alt=""/>Best Affiliate Marketing Conferences
+with CPA Networks in 2023</p><hr/>
+<p className="text-sm text-white font-bold my-3 capitalize flex items-start gap-2"><Image src={'/icons/RightButton.svg' } width={24} height={24} alt=""/>Best Affiliate Marketing Conferences
+with CPA Networks in 2023</p>
+
+       
+      </div>
+      <div className="bg-[#d3d3d3] rounded-2xl py-[26px] px-[45px] mb-[48px]">
+        <h4 className="text-[#1F57A1] uppercase text-lg font-bold border-b-[1px] border-[#1F57A1] inline-block w-auto mb-5">
+          Tags
+        </h4>
+        <div className="flex flex-wrap gap-4">
+            <p className="bg-[#D9D9D9] text-base font-light px-2 py-1 capitalize rounded-[4px]"># affiliate forex</p>
+            <p className="bg-[#D9D9D9] lighttext-base font-light px-2 py-1 capitalize rounded-[4px]">#  broken affiliate</p>
+            <p className="bg-[#D9D9D9] text-base font-light px-2 py-1 capitalize rounded-[4px]"># affiliate program forex</p>       
+            </div> 
+      </div>
+      <div className="bg-[#1F57A1] rounded-2xl py-[26px] px-[45px] mb-[36px]">
+      <h4 className="text-white uppercase text-lg font-bold border-b-[1px] border-[#fff] inline-block w-auto mb-5">
+          Recent Comments
+        </h4>
+       <p className="text-base text-white font-light mb-3 capitalize">There are many variations of passages</p>
+       <p className="text-base text-white font-light mb-3 capitalize">If you are going to use a passage</p>
+       <p className="text-base text-white font-light mb-3 capitalize">you need to be sure</p>
+       <p className="text-base text-white font-light mb-3 capitalize">you need to be sure</p>
+       <p className="text-base text-white font-light mb-3 capitalize">you need to be sure</p>
+      </div>
+    </div>
+  );
+};
+
+export default BlogSidebar;
